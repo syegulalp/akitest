109b22b0d0528969ec19765b1fd234d60fcb7071
Serdar Yegulalp <serdar@genjipress.com>
2020-12-19 13:52:11 -0500

    fix tests

--- test/test_02.py
+++ test/test_02.py
@@ -6,8 +6,6 @@ class TestExpressions(BaseTest):
     def test_int_if(self):
         self.eq("if 2==2 1 else 0", 1)
         self.eq("if 2!=2 1 else 0", 0)
-<<<<<<< HEAD
-=======
 
     def test_bool_if(self):
         self.eq("if True 1 else 0", 1)
@@ -22,4 +20,3 @@ class TestExpressions(BaseTest):
         self.ex("if 2==True 1 else 0", AkiTypeException)
         self.ex("if 2==True 1 else False", AkiTypeException)
         self.ex("if 2==2 1 else False", AkiTypeException)
->>>>>>> dev
--- test/utils.py
+++ test/utils.py
@@ -3,14 +3,19 @@ from codegen import codegen
 from jitengine import jit
 import unittest
 
-
+main_func_name="main"
 class BaseTest(unittest.TestCase):
+    #incr = 0
+
     def cmd(self, command):
         ast = parser.parse(command)
-        codegen.gen(ast)
-        # print(str(codegen.module))
-        result = jit.execute(codegen)
+        #main_func_name = f"main_{BaseTest.incr}"
+        codegen.gen(ast, main_func=main_func_name)
+        #print (str(codegen.module))
+        #print (codegen.return_value.aki)
+        result = jit.execute(codegen, entry_point=main_func_name)
         jit.clear()
+        #BaseTest.incr+=1
         return result
 
     def eq(self, command, assertion):
