74aa2016f07d022eca10ea37ca27944d1da3b707
Serdar Yegulalp <serdar@genjipress.com>
2020-12-22 10:37:06 -0500

    add to demo

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ from parsing import parser
 from codegen import codegen
 from jitengine import jit
 
-commands = ["True", "2", "2+2", "2==2", "2==4", "2.0", "if 32==2 0 else 1", "when 32 0 else 1",]
+commands = ["True", "2", "2+2", "2==2", "2==4", "2.0", "if 32==2 0 else 1", "when 32 0 else 1","def test(){32} if test()==32 0 else 1"]
 
 for command in commands:
     print ("IN >",command)
