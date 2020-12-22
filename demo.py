95deddb799f0074fc36fe2c201fce29c56d64d52
Serdar Yegulalp <serdar@genjipress.com>
2020-12-22 10:22:00 -0500

    fix demo

--- demo.py
+++ demo.py
@@ -6,14 +6,15 @@ from parsing import parser
 from codegen import codegen
 from jitengine import jit
 
-commands = ["True", "2", "2+2", "2==2", "2==4", "2.0", "if 32==2 0 else 1", "when 32 0 else 1"]
+commands = ["True", "2", "2+2", "2==2", "2==4", "2.0", "if 32==2 0 else 1", "when 32 0 else 1",]
 
 for command in commands:
     print ("IN >",command)
-    ast = parser.parse(command, start="statement")
+    ast = parser.parse(command, start="immediate")
     #print(ast)
     codegen.gen(ast)
     #print(str(codegen.module))
-    result = jit.execute(codegen)
+    entry =  codegen.anon_counter()
+    result = jit.execute(codegen, entry_point = entry)
+    print("OUT>", result, codegen.return_value(entry).aki)
     jit.clear()
-    print("OUT>", result, codegen.return_value.aki)
