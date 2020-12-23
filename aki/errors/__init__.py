9e0d7002636c6e5b0d9c55919495fe7282cf0015
Serdar Yegulalp <serdar@genjipress.com>
2020-12-23 01:13:29 -0500

    more robust repl error handling

--- aki/errors/__init__.py
+++ aki/errors/__init__.py
@@ -7,8 +7,28 @@ class QuitException(Exception):
 
 
 class AkiBaseException(Exception):
+    def __init__(self, txt, node, err=None):
+        self.txt = txt
+        self.node = node
+        self.err = err
+
+    @property
+    def err_type(self):
+        return self.__class__.__name__[3:]
+
+    def __str__(self):
+        lines = self.txt.split()
+        line = self.node.line - 1
+        return f"{self.err_type}: (line {self.node.line}, col {self.node.column}) {self.err}\n{lines[line]}\n{'-'*(self.node.column-1)}^"
+
+
+class AkiSyntaxError(AkiBaseException):
     pass
 
 
-class AkiTypeException(AkiBaseException):
+class AkiTypeError(AkiBaseException):
+    pass
+
+
+class AkiNameError(AkiBaseException):
     pass
